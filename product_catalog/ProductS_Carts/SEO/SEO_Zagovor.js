77a67d5d10f7019a447906bbbbe8928aa5195b7d
Stanislaa <112971669+Stanislaa@users.noreply.github.com>
2025-10-15 21:10:34 +0300

    Add files via upload

---
+++ product_catalog/ProductS_Carts/SEO/SEO_Zagovor.js
@@ -0,0 +1,23 @@
+const button_favorite = document.querySelector('#button_favorite');
+
+button_favorite.addEventListener('click', function() {
+    const SEO = {
+        name: 'SEO-Заговор',
+        price: '399$',
+        image: "../product_catalog/ProductS_Carts/SEO/content/keys.png"
+    };
+
+    let favorite = JSON.parse(localStorage.getItem('favorite'));
+    favorite.push(SEO);
+    localStorage.setItem('favorite', JSON.stringify(favorite));
+
+
+    alert('Товар добавлен в избранное');
+    updateFavoriteCounter();
+});
+
+function updateFavoriteCounter() {
+    const favorite = JSON.parse(localStorage.getItem('favorite'));
+    const itemCounter = document.getElementById('item_counter');
+    itemCounter.textContent = `В ИЗБРАННОМ ${favorite.length} ТОВАРОВ`;
+}
