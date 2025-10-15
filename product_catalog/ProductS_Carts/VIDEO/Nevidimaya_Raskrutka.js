1f83174e7920855dc17bdfa69c1f6d123a3e7a0b
Stanislaa <112971669+Stanislaa@users.noreply.github.com>
2025-10-15 21:11:52 +0300

    Add files via upload

---
+++ product_catalog/ProductS_Carts/VIDEO/Nevidimaya_Raskrutka.js
@@ -0,0 +1,23 @@
+const button_favorite = document.querySelector('#button_favorite');
+
+button_favorite.addEventListener('click', function() {
+    const Video = {
+        name: 'Невидимая раскрутка',
+        price: '249$',
+        image: "../product_catalog/ProductS_Carts/VIDEO/content/video.png"
+    };
+
+    let favorite = JSON.parse(localStorage.getItem('favorite'));
+    favorite.push(Video);
+    localStorage.setItem('favorite', JSON.stringify(favorite));
+
+
+    alert('Товар добавлен в избранное');
+    updateFavoriteCounter();
+});
+
+function updateFavoriteCounter() {
+    const favorite = JSON.parse(localStorage.getItem('favorite'));
+    const itemCounter = document.getElementById('item_counter');
+    itemCounter.textContent = `В ИЗБРАННОМ ${favorite.length} ТОВАРОВ`;
+}
