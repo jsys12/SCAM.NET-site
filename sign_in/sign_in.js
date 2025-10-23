325aa33fa01525ad62b089d98d71d0aa9054b974
jsys12 <soloveuseva4@gmail.com>
2025-10-23 10:25:39 +0300

    Add files via upload

--- sign_in/sign_in.js
+++ sign_in/sign_in.js
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
         data["username"] = formData.get("username");
         data["password"] = formData.get("password");
         
-        fetch ( "http://127.0.0.1:8000/auth/", {
+        fetch ( "https://my-fastapi-app-tfwi.onrender.com/auth/", {
             method: "POST",
             headers: {
                 "Content-Type" : "application/json"
